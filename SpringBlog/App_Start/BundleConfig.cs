26681596bb8e67e0faca9fc4321351ec5c93e6b9
byoslab <55752032+byoslab@users.noreply.github.com>
2020-04-29 18:32:13 +0300

    Layout, Bundle düzenlemeleri

--- SpringBlog/App_Start/BundleConfig.cs
+++ SpringBlog/App_Start/BundleConfig.cs
@@ -8,23 +8,28 @@ namespace SpringBlog
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.UseCdn = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "https://code.jquery.com/jquery-3.4.1.min.js").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new StyleBundle("~/Content/bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css").Include(
+                      "~/Content/bootstrap.css"));  // hazır minified bundle kullanmak icin sadece "Bundle" yazılır. gelistirirken bootscss/bootstrap.css i kullan. yayınlarken CDN'i kullan.
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));  //contentin icinde olmayan sanal bi klasor olustur - css
+
+            #if DEBUG  //normalde gerek yok, release/debug farkını gostermek icin yapıldı.
+                BundleTable.EnableOptimizations = false; //bundle'ları minified duruma getirir.
+            #else
+                BundleTable.EnableOptimizations = true;
+            #endif
         }
     }
 }
